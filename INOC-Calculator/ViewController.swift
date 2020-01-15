29caf0dd1eb26140945fd88fa03d28949503f861
Anita Khounthong <anita.khounthong@gmail.com>
2020-01-15 12:07:50 +0100

    deleted comment from felix

--- INOC-Calculator/ViewController.swift
+++ INOC-Calculator/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //Hi Anita this is a test comment
+       
     }
 
 
