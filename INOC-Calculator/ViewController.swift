33f10ebaa9827a16ff220b93b0cc25fcd95d1466
Felix Prünte <f.pruente@xu-university.de>
2020-01-15 11:36:42 +0100

    Initial Commit

---
+++ INOC-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INOC-Calculator
+//
+//  Created by FelixP on 15.01.20.
+//  Copyright © 2020 XU Exponential University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
