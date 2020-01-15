bedfcea41c6cc0751784c3f7fc9d12371a4921f3
Felix Prünte <f.pruente@xu-university.de>
2020-01-15 12:06:02 +0100

    Added a comment to show anita how git repos work.

--- INOC-Calculator/ViewController.swift
+++ INOC-Calculator/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Hi Anita this is a test comment
     }
 
 
