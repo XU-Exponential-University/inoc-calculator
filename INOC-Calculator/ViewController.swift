8c2131a28a994856eca5d02805e5e7559dae8e6e
Anita Khounthong <anita.khounthong@gmail.com>
2020-01-20 16:32:42 +0100

    cleared unused variables, changed functions of operators to support calculation of more than 2 numbers, cleared percentage function (did not work properly anymore), deleted the getnumber() function

--- INOC-Calculator/ViewController.swift
+++ INOC-Calculator/ViewController.swift
@@ -9,20 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-//    This array was not used in caculation, just for viewing in the console:
-    var numbersArray = [Double]()
-    
-    var currentStateOfCalculation = 0.0
     
     var numberOnScreen = ""
     
-    var previousNumber = ""
-    
-//    var newNumber = ""
-    
-    var calculation = [String]()
-    
     var currentOperator = ""
+    
+    var currentNumber = ""
+    
+    var calculationString = ""
+    
+    var result = 0.0
     
     @IBOutlet weak var resultLabel: UILabel!
     
@@ -32,15 +28,12 @@ class ViewController: UIViewController {
     @IBAction func clearButtonClicked(_ sender: Any) {
         
         clearText()
-        numbersArray = [Double]()
-        currentStateOfCalculation = 0.0
-        previousNumber = ""
-//        newNumber = ""
-        calculation = [String]()
+        currentNumber = ""
         currentOperator = ""
         resultLabel.text = "0"
+        calculationString = ""
         
-        unblockOperatorButton(unblock: previousButton)
+        unblockOperatorButton()
         previousButton = UIButton()
     }
     
@@ -49,30 +42,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func percentageButtonClicked(_ sender: UIButton){
-        // if the result label is emppty(when we haven't typed a number), it should have the value of 0:
-        if numberOnScreen == "" {
-            numberOnScreen = "0"
-        }
-        //  converting the String to a double:
-        let convertedNumber = Double(numberOnScreen)!
-//        if there is no operator involved and % is clicked, the number will be just devided by 100, else the number will be calculated as percentage from the previous number:
-        if currentOperator == "" || currentOperator == "=" {
-            if currentOperator == "" {
-                currentStateOfCalculation = convertedNumber
-            }
-            currentStateOfCalculation = currentStateOfCalculation / 100
-            numberOnScreen = String(currentStateOfCalculation)
-        } else {
-            let percentage = currentStateOfCalculation * convertedNumber / 100
-            numberOnScreen =  String(percentage)
-        }
-        resultLabel.text = numberOnScreen
+        
         
     }
     
     @IBAction func numberZeroButtonClicked(_ sender: UIButton){
         if resultLabel.text != "0" {
-            getNumber(of: sender)
+            numberOnScreen += sender.currentTitle!
+            
+            resultLabel.text = numberOnScreen
         }
     }
     
@@ -83,42 +61,58 @@ class ViewController: UIViewController {
             resultLabel.text = numberOnScreen
         }
         else {
-            getNumber(of: sender)
-            unblockOperatorButton(unblock: previousButton)
+            numberOnScreen += sender.currentTitle!
+            
+            resultLabel.text = numberOnScreen
+            unblockOperatorButton()
         }
     }
     
-//    4 operators are connected from the storyboard here: (multiply, devide, plus, minus)
+    //    4 operators are connected from the storyboard here: (multiply, devide, plus, minus)
     @IBAction func operatorClicked(_ sender: UIButton){
-        calculationWithOperators(of: sender.currentTitle!)
+        
+        if numberOnScreen.contains(".") == false && currentOperator != "=" {
+            numberOnScreen += ".0"
+        }
+        currentOperator = " \(sender.currentTitle!) "
+        calculationString += numberOnScreen + currentOperator
+        print(calculationString)
+       
+        clearText()
         
         blockOperatorButton(block: sender)
         
         if previousButton != UIButton() {
-            unblockOperatorButton(unblock: previousButton)
+            unblockOperatorButton()
         }
         previousButton = sender
     }
     
+
     @IBAction func resultButtonClicked(_ sender: UIButton){
-        calculationWithOperators(of: sender.currentTitle!)
-        //        numberOnScreen = String(format: "%.1f", currentStateOfCalculation)
-        //        currentOperator = ""
-        unblockOperatorButton(unblock: previousButton)
-        previousButton = UIButton()
+        calculationString += numberOnScreen
+        currentOperator = "="
+        print(calculationString)
+        calculationString = calculationString.replacingOccurrences(of: "x", with: "*")
+        let y = NSExpression(format:calculationString)
+        
+        result = y.expressionValue(with: nil, context: nil) as! Double
+        print(result)
+        resultLabel.text = "\(result)"
+        calculationString = "\(result)"
+        clearText()
+        unblockOperatorButton()
     }
     
     //    This function is connected to the buttons of number 1 to 9 from the storyboard:
     @IBAction func numberButtonClicked(_ sender: UIButton){
-        getNumber(of: sender)
-        unblockOperatorButton(unblock: previousButton)
-    }
-//    This takes the string(title) of the button and put it in the result label
-    func getNumber(of button: UIButton) {
-        numberOnScreen += button.currentTitle!
+        if currentOperator == "=" {
+            calculationString = ""
+        }
+        numberOnScreen += sender.currentTitle!
         
         resultLabel.text = numberOnScreen
-        
+        unblockOperatorButton()
     }
     
     //    This function clears the text on the Label and enables the decimal BUtton
@@ -127,52 +121,6 @@ class ViewController: UIViewController {
         decimalButton.isEnabled = true
     }
     
-    //    operations are not performed when clicked, but stored to be operated after the next number is clicked, and the next operator is clicked
-    func calculationWithOperators(of buttonTitle: String){
-        print("numberonscreen before \(numberOnScreen)")
-        
-        if numberOnScreen == "" { numberOnScreen = "0" }
-        let convertedNumber = Double(numberOnScreen)!
-        numbersArray.append(convertedNumber)
-        print(numbersArray)
-        print("c: \(currentOperator)")
-        print("numberonscreen \(numberOnScreen)")
-        
-        switch currentOperator {
-        case "+":
-            currentStateOfCalculation += convertedNumber
-            currentOperator = buttonTitle
-            print("real \(currentOperator)")
-        case "-":
-            currentOperator = buttonTitle
-            currentStateOfCalculation -= convertedNumber
-            
-        case "x":
-            currentStateOfCalculation *= convertedNumber
-            print("real \(currentOperator)")
-            currentOperator = buttonTitle
-            
-        case "/":
-            if currentStateOfCalculation == 0.0 && numbersArray.count == 1 {
-                currentStateOfCalculation = convertedNumber
-            } else {
-                currentStateOfCalculation /= convertedNumber }
-            currentOperator = buttonTitle
-        case "":
-            currentStateOfCalculation = convertedNumber
-            currentOperator = buttonTitle
-            
-        case "=":
-            currentOperator = buttonTitle
-        default:
-            print("error calculation")
-        }
-//        print("current \(currentStateOfCalculation)")
-        resultLabel.text = String(currentStateOfCalculation)
-        //        fix , after clicking x and then + numberonscreen = ""
-        clearText()
-        
-    }
     //    empty Button
     var previousButton = UIButton()
     //    This function should block operator from being executed twice unnessasarily:
@@ -180,7 +128,7 @@ class ViewController: UIViewController {
     func blockOperatorButton(block button: UIButton) {
         button.isEnabled = false
     }
-    func unblockOperatorButton(unblock button: UIButton) {
+    func unblockOperatorButton() {
         previousButton.isEnabled = true
     }
     
